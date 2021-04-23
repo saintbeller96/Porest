d1d91fe72951f709e8957b8f9535dcc8de2bce1a
윤기현 <ykh094@naver.com>
2021-04-23 15:28:28 +0900

    feat : SwaggerConfig에 Authorization 글로벌 파라메터 추가

--- backend/src/main/java/com/hanmaum/counseling/config/SwaggerConfig.java
+++ backend/src/main/java/com/hanmaum/counseling/config/SwaggerConfig.java
@@ -2,18 +2,36 @@ package com.hanmaum.counseling.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket api() {
+        ParameterBuilder parameterBuilder = new ParameterBuilder();
+        parameterBuilder.name("Authorization") //헤더 이름
+                .description("Access Tocken으로 로그인하면 발급됩니다. 앞에 꼭 \"Bearer \" 붙여주세요.") //설명
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+
+        List<Parameter> parameters = new ArrayList<>();
+        parameters.add(parameterBuilder.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(parameters)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
