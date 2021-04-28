9edc6dfd79caa302e53873d9cdf9dbb24b09956a
sieun <najse77@naver.com>
2021-04-28 16:20:28 +0900

    style : Add assets files

--- frontend/src/views/mailbox/MyMailboxPage.vue
+++ frontend/src/views/mailbox/MyMailboxPage.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-    내 우체통 페이지
+    <img src="@/assets/image/flower.jpg" class="background" />
+    <div class="mailbox-container">
+      <img src="@/assets/image/mailbox.png" class="mailbox" />
+    </div>
+    <p>내 우체통 페이지</p>
     <p>편지 리스트 보여지는 곳</p>
+    <button @click="goToLetterDetail">편지 디테일 이동</button>
     <div>
-      <button @click="goToLetterDetail">편지 디테일 이동</button>
+      <img src="@/assets/image/envelope.png" class="envelope" />
     </div>
   </div>
 </template>
@@ -18,4 +23,40 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.mailbox-container {
+  display: flex;
+  justify-content: center;
+}
+
+.mailbox {
+  position: absolute;
+  width: 25vw;
+  pointer-events: none;
+  margin-top: 35vh;
+}
+
+p {
+  position: relative;
+  font-size: 1.5em;
+  color: white;
+}
+
+button {
+  position: relative;
+}
+
+.envelope {
+  position: absolute;
+  width: 10vw;
+}
+</style>
