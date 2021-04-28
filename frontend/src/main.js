c7b7ee18ed3b445128aa4b43802e0a3c45fcf0c4
yongsu kwon <yskwon0619@gmail.com>
2021-04-28 23:49:22 +0900

    feat : Create design page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
+import * as THREE from 'three';
+import * as TrackballControls from 'three-trackballcontrols';
+import * as ThreeStats from 'three-stats';
 Vue.config.productionTip = false;
+
+Vue.prototype.THREE = THREE;
+Vue.prototype.TrackballControls = TrackballControls;
+Vue.prototype.ThreeStats = ThreeStats;
 
 new Vue({
   router,
---
+++ frontend/src/views/test/3D.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="threed-wrapper">
+    <div class="scene">
+      <img src="@/assets/image/34132.eps" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '3D',
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped src="@/assets/css/test/3D.css"></style>
