373e9ee3e9e9c5b086147d0715751f1bc254a5c4
강민지 <hlery1206@naver.com>
2021-05-10 03:02:32 +0900

    design : Mail box page

--- frontend/src/views/mailbox/MyMailboxPage.vue
+++ frontend/src/views/mailbox/MyMailboxPage.vue
@@ -1,15 +1,8 @@
 <template>
-  <div>
-    <img src="@/assets/image/flower.jpg" class="background" />
-    <div class="mailbox-container">
-      <img src="@/assets/image/mailbox.png" class="mailbox" />
-    </div>
-    <p>내 우체통 페이지</p>
-    <p>편지 리스트 보여지는 곳</p>
-    <button @click="goToLetterDetail">편지 디테일 이동</button>
-    <div>
-      <img src="@/assets/image/envelope.png" class="envelope" />
-    </div>
+  <div class="mailbox-wrapper">
+   
+    <!-- <button @click="goToLetterDetail">편지 디테일 이동</button> -->
+    
   </div>
 </template>
 
@@ -24,39 +17,6 @@ export default {
 </script>
 
 <style scoped>
-.background {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
 
-.mailbox-container {
-  display: flex;
-  justify-content: center;
-}
 
-.mailbox {
-  position: absolute;
-  width: 25vw;
-  pointer-events: none;
-  margin-top: 35vh;
-}
-
-p {
-  position: relative;
-  font-size: 1.5em;
-  color: white;
-}
-
-button {
-  position: relative;
-}
-
-.envelope {
-  position: absolute;
-  width: 10vw;
-}
 </style>
