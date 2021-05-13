7bf070186aa18fdbd59089d101335785fed3f058
강민지 <hlery1206@naver.com>
2021-05-13 14:25:36 +0900

    feat: update postmain

---
+++ frontend/mj_design/PostPage/postmain/postmain2.js
@@ -0,0 +1,13 @@
+//var img = document.getElementById("image");
+//var transformed = false;
+//img.onclick = function(){
+//  if(transformed == false) {
+//    this.classList.add('transform');
+//    transformed = true;
+//  }
+//  else {
+//    this.classList.remove('transform');
+//    transformed = false;
+//  }
+//
+//}
